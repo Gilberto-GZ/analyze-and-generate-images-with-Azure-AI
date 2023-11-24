cb53e4e6b62888cae5338e5790a95d399bd3201d
Gilberto-GZ <ggz2489@hotmail.com>
2023-11-24 16:54:12 -0600

    GUI Created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleAnalyzeImage = () => {
+    // Add logic to analyze the image
+    console.log('Analyze image with URL:', imageUrl);
+  };
+
+  const handleGenerateImage = () => {
+    // Add logic to generate image
+    console.log('Generate image with URL:', imageUrl);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Computer Vision</h1>
+        <input
+          type="text"
+          placeholder="Enter image URL"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <button onClick={handleAnalyzeImage}>Analyze Image</button>
+        <button onClick={handleGenerateImage}>Generate Image</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
